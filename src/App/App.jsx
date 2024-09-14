461cb9f02d10051c51d21a805b698c6ab11590dc
unknown <bbb155072@gmail.com>
2024-09-14 15:50:08 +0300

    first commit

---
+++ src/App/App.jsx
@@ -0,0 +1,21 @@
+import Weather from "../weather/weather";
+import Map from '../map/map'
+
+import s from './index.module.css'
+import '../commonStyles.css'
+
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+const App = () => {
+
+    return (
+        <div className={`${s.container} `}>
+            <Routes>
+                <Route path="/" element={<Weather />} />
+                <Route path="/Map" element={<Map />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
